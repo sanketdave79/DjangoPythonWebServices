3de6703bfea9ff26b28c633458c044287548288d
sanketdave79 <sanketdave79@techmentaion.com>
2016-06-29 22:03:33 +1000

    Removing unnecessary imports

--- api/views.py
+++ api/views.py
@@ -1,19 +1,9 @@
 
-import json
-import django.core.serializers
 from django.shortcuts import render
 from models import Beer
-from django.utils import simplejson
 from django.http import HttpResponse
-from bson import json_util
-from pprint import pprint
-
 
 
 def index(request):
 	beers = Beer.objects().to_json()
-	#customers = django.core.serializers.serialize('json',[customers,])
-	
-	
 	return HttpResponse(beers)
-	
